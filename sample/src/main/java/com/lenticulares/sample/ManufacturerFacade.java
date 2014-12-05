5ac4f94b82173f80fd0d23795aa23799e5980dbb
danielfuentes <giracros@gmail.com>
2014-12-04 22:07:57 -0500

    Funciona Full HD :D

---
+++ sample/src/main/java/com/lenticulares/sample/ManufacturerFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lenticulares.sample;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author danielfuentes
+ */
+@Stateless
+public class ManufacturerFacade extends AbstractFacade<Manufacturer> {
+    @PersistenceContext(unitName = "com.lenticulares_sample_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ManufacturerFacade() {
+        super(Manufacturer.class);
+    }
+    
+}
